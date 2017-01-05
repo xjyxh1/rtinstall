fb649b8a4dfbe9ccece469d6a45ac34d4f57b660
xjyxh1 <jianyu.xu@northumbria.ac.uk>
2017-01-05 15:38:10 +0000

    Update ipv6conf.sh

--- ipv6conf.sh
+++ ipv6conf.sh
@@ -1,9 +1,9 @@
 echo "interface "eno1" {" >>  /etc/dhcp/dhclient6.conf
-echo "   send dhcp6.client-id 00:03:00:01:5a:dc:aa:fc:73:41;" >>  /etc/dhcp/dhclient6.conf
+echo "   send dhcp6.client-id 00:03:00:01:bc:74:1f:93:08:60;" >>  /etc/dhcp/dhclient6.conf
 echo "}" >>  /etc/dhcp/dhclient6.conf
 
 echo "[Unit]" >> /etc/systemd/system/dhclient.service
-echo "Description=dhclient for sending 00:03:00:01:5a:dc:aa:fc:73:41 IPv6" >> /etc/systemd/system/dhclient.service
+echo "Description=dhclient for sending 00:03:00:01:bc:74:1f:93:08:60 IPv6" >> /etc/systemd/system/dhclient.service
 echo "Wants=network.target" >> /etc/systemd/system/dhclient.service
 echo "Before=network.target" >> /etc/systemd/system/dhclient.service
 echo "" >> /etc/systemd/system/dhclient.service
@@ -17,7 +17,7 @@ echo "WantedBy=multi-user.target" >> /etc/systemd/system/dhclient.service
 systemctl enable dhclient.service
 
 echo "iface eno1 inet6 static" >> /etc/network/interfaces
-echo "    address 2001:bc8:2664:100::1" >> /etc/network/interfaces
+echo "    address 2001:bc8:2664:200::1" >> /etc/network/interfaces
 echo "    netmask 56" >> /etc/network/interfaces
 
 ip6tables -A OUTPUT -p udp --dport 547 -m limit --limit 10/min --limit-burst 5 -j ACCEPT
