ff6ea4a7ad0f7551075f49c79773294dcd51325a
xjyxh1 <jianyu.xu@northumbria.ac.uk>
2017-01-05 13:59:29 +0000

    Create bbr2.sh

---
+++ bbr2.sh
@@ -0,0 +1,5 @@
+echo "net.core.default_qdisc=fq" >> /etc/sysctl.conf
+echo "net.ipv4.tcp_congestion_control=bbr" >> /etc/sysctl.conf
+sysctl -p
+sysctl net.ipv4.tcp_available_congestion_control
+lsmod | grep bbr
