1308d1a5ea0e44978d1b8dbe2c441a504ee9125a
xjyxh1 <jianyu.xu@northumbria.ac.uk>
2017-01-11 17:34:34 +0000

    Update bbr1.sh

--- bbr1.sh
+++ bbr1.sh
@@ -1,5 +1,5 @@
 wget http://kernel.ubuntu.com/~kernel-ppa/mainline/v4.9.2/linux-headers-4.9.2-040902_4.9.2-040902.201701090331_all.deb
 wget http://kernel.ubuntu.com/~kernel-ppa/mainline/v4.9.2/linux-headers-4.9.2-040902-generic_4.9.2-040902.201701090331_amd64.deb
 wget http://kernel.ubuntu.com/~kernel-ppa/mainline/v4.9.2/linux-image-4.9.2-040902-generic_4.9.2-040902.201701090331_amd64.deb
-dpkg -i linux-image-4.9.0*.deb
+dpkg -i linux-image-4.9.2*.deb
 update-grub
