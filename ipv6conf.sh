623597f3b5acba76419ee4b6153ef4a3cae065f9
xjyxh1 <jianyu.xu@northumbria.ac.uk>
2017-01-19 17:06:20 +0000

    Update ipv6conf.sh

--- ipv6conf.sh
+++ ipv6conf.sh
@@ -7,8 +7,8 @@ read -p "Your IPv6 block address (e.g. 2001:bb8:3e23:200::1): " BLOCK_ADDR
 read -p "Associated DUID (e.g. 00:03:00:00:34:b0:0c:47:4a:0e): " BLOCK_DUID
 
 echo "interface "$INTERFACE" {" >>  /etc/dhcp/dhclient6.conf
-echo "   send dhcp6.client-id $BLOCK_DUID;" >>  /etc/dhcp/dhclient6.conf
+echo "   send dhcp6.client-id "$BLOCK_DUID";" >>  /etc/dhcp/dhclient6.conf
 echo "}" >>  /etc/dhcp/dhclient6.conf
 
-dhclient -cf /etc/dhcp/dhclient6.conf -6 -P -v $INTERFACE
-/sbin/ifconfig $INTERFACE inet6 add $BLOCK_ADDR/56
+dhclient -cf /etc/dhcp/dhclient6.conf -6 -P -v "$INTERFACE"
+/sbin/ifconfig $INTERFACE inet6 add "$BLOCK_ADDR"/56
