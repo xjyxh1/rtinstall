b34ad5d50b94268cdca197391584af925413a2c2
xjyxh1 <jianyu.xu@northumbria.ac.uk>
2017-01-19 13:29:24 +0000

    Update ipv6conf.sh

--- ipv6conf.sh
+++ ipv6conf.sh
@@ -10,23 +10,6 @@ echo "interface "$INTERFACE" {" >>  /etc/dhcp/dhclient6.conf
 echo "   send dhcp6.client-id $BLOCK_DUID;" >>  /etc/dhcp/dhclient6.conf
 echo "}" >>  /etc/dhcp/dhclient6.conf
 
-echo "[Unit]" >> /etc/systemd/system/dhclient.service
-echo "Description=dhclient for sending $BLOCK_DUID IPv6" >> /etc/systemd/system/dhclient.service
-echo "Wants=network.target" >> /etc/systemd/system/dhclient.service
-echo "Before=network.target" >> /etc/systemd/system/dhclient.service
-echo "" >> /etc/systemd/system/dhclient.service
-echo "[Service]" >> /etc/systemd/system/dhclient.service
-echo "Type=forking" >> /etc/systemd/system/dhclient.service
-echo "ExecStart=/usr/sbin/dhclient -cf /etc/dhcp/dhclient6.conf -6 -P -v $INTERFACE" >> /etc/systemd/system/dhclient.service
-echo "" >> /etc/systemd/system/dhclient.service
-echo "[Install]" >> /etc/systemd/system/dhclient.service
-echo "WantedBy=multi-user.target" >> /etc/systemd/system/dhclient.service
-
-systemctl enable dhclient.service
-
-echo "iface $INTERFACE inet6 static" >> /etc/network/interfaces
-echo "    address $BLOCK_ADDR" >> /etc/network/interfaces
-echo "    netmask 56" >> /etc/network/interfaces
-
-ip6tables -A OUTPUT -p udp --dport 547 -m limit --limit 10/min --limit-burst 5 -j ACCEPT
-ip6tables -A OUTPUT -p udp --dport 547 -j DROP
+dhclient -cf /etc/dhcp/dhclient6.conf -6 -P -v $INTERFACE
+/sbin/ifconfig $INTERFACE inet6 add $BLOCK_ADDR/56
+ping6 ipv6.google.com
