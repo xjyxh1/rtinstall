9adce9ee0a3ee2c77320ea5230c14ecc3d81f33f
xjyxh1 <jianyu.xu@northumbria.ac.uk>
2017-10-21 00:38:19 +0100

    Create drivers.sh

---
+++ drivers.sh
@@ -0,0 +1,5 @@
+cd /lib/firmware
+mkdir bnx2
+cd bnx2
+wget https://github.com/xjyxh1/rtinstall/raw/master/bnx2/bnx2-mips-09-6.2.1b.fw
+wget https://github.com/xjyxh1/rtinstall/raw/master/bnx2/bnx2-mips-06-6.2.3.fw
